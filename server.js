80bb13f95924a5f9af89a7643991ff256715015a
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-17 11:53:06 -0500

    add get requests

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const PORT = 5000;
 // there are thousands of ports, between 1 and 9999;
 // some are assigned to special things, but many are safe for them to use
 
+// req is request. res is response.
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/hey', function (req, res) {
+    res.send('Howdy, partner');
+});
+
 // in order to be a server, it needs to be constantly listening
 // we will assign a port number to which to listen
-app.listen(PORT);
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`);
+});
+
+
+// usually websites respond with html
 
